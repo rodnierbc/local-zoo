f4d85c2b79072835e1cd098d491c989e4800b580
Rodnier Borrego <rodnierbc89@gmail.com>
2018-02-23 10:14:46 -0800

    Create root component

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,7 +11,8 @@ import { Animal } from './models/animal.model';
 export class AppComponent {
 
   animals: Animal[] = [
-    new Animal('Arctic Fox', 'Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool shade', 'Loud noises')
+    new Animal('Arctic Fox', 'Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool shade', 'Loud noises'),
+    new Animal('Ocelot', 'Prince', 4, 'Carnivore', 'Tropical Rain Forest Building', 6, 'Male', 'Laying in the sunshine', 'Toys that are not rope-based')
   ];
 
 }
