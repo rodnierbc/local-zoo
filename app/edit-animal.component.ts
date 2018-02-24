4f33a14d40a1eacbf35ffd66625fd797d1b027c0
Rodnier Borrego <rodnierbc89@gmail.com>
2018-02-23 19:34:06 -0800

    added some styleson edit-animal component template

--- app/edit-animal.component.ts
+++ app/edit-animal.component.ts
@@ -4,21 +4,51 @@ import { Animal } from './models/animal.model';
 @Component({
   selector: 'edit-animal',
   template: `
-    <div>
         <div *ngIf="selectedAnimal">
-          <hr>
-          <label>Enter animal name:</label>
-          <input [(ngModel)]="selectedAnimal.name">
-          <label>Enter animal age:</label>
-          <input [(ngModel)]="selectedAnimal.age">
-          <label>Enter animal caretakers:</label>
-          <input [(ngModel)]="selectedAnimal.caretakers">
-          <button (click)="doneEdition()">Edit</button>
+          <div class="card">
+            <div class="alert alert-success">
+              <div class="row">
+                  <div class="col-md-10">
+                      <div class="form-group">
+                          <h3>Edit Animal</h3>
+                      </div>
+                  </div>
+              </div>
+            </div>
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-md-4">
+                      <div class="form-group">
+                          <label>Name</label>
+                          <input class="form-control input-sm" type="text" [(ngModel)]="selectedAnimal.name">
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="form-group">
+                          <label>Age</label>
+                          <input class="form-control input-sm" type="number" [(ngModel)]="selectedAnimal.age">
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="form-group">
+                          <label>Caretakers Number</label>
+                          <input class="form-control input-sm" type="number" [(ngModel)]="selectedAnimal.caretakers">
+                      </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="row">
+                      <div class="col-md-3">
+                          <button class="btn btn-success btn-xs" (click)="doneEdition()">Update</button>
+                      </div>
+                  </div>
+                </div>
+
+              </div>
+          </div>
         </div>
-      </div>
   `
 })
-
 export class EditAnimalComponent {
     @Input() selectedAnimal: Animal;
     @Output() doneEditionSender = new EventEmitter();
