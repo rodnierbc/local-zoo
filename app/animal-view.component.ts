d8abc4bf5dc7dea74c12c13fb6b7a687f1fa0c95
Rodnier Borrego <rodnierbc89@gmail.com>
2018-02-23 19:01:28 -0800

    added some styles on animal-view component template

--- app/animal-view.component.ts
+++ app/animal-view.component.ts
@@ -6,9 +6,33 @@ import { Animal } from './models/animal.model';
   template: `
     <div>
         <div *ngIf="selectedAnimalView">
-          <hr>
-          <p>name: {{selectedAnimalView.name}}</p>
-          <button (click)="doneView()">Exit</button>
+          <div class="card">
+            <div class="alert alert-Warning">
+              <div class="row">
+                  <div class="col-md-10">
+                      <div class="form-group">
+                          <h3>Animal Details</h3>
+                      </div>
+                  </div>
+                  <div class="col-md-2">
+                      <div class="form-group">
+                          <button class="btn btn-Warning btn-xs" (click)="doneView()">Exit</button>
+                      </div>
+                  </div>
+              </div>
+            </div>
+              <div class="card-body">
+                <p><strong>Species:</strong> {{selectedAnimalView.species}}</p>
+                <p><strong>name:</strong> {{selectedAnimalView.name}}</p>
+                <p><strong>Age:</strong> {{selectedAnimalView.age}} year(s) old</p>
+                <p><strong>Diet:</strong> {{selectedAnimalView.diet}}</p>
+                <p><strong>Location:</strong> {{selectedAnimalView.location}}</p>
+                <p><strong>Caretakers:</strong> {{selectedAnimalView.caretakers}}</p>
+                <p><strong>Sex:</strong> {{selectedAnimalView.sex}}</p>
+                <p><strong>Likes:</strong> {{selectedAnimalView.likes}}</p>
+                <p><strong>Dislikes:</strong> {{selectedAnimalView.dislikes}}</p>
+              </div>
+          </div>
         </div>
       </div>
   `
