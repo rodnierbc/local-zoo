a815a51950adf6868f517ee788e3b06cc301821f
Rodnier Borrego <rodnierbc89@gmail.com>
2018-02-23 17:37:06 -0800

    Create a table for to show animals in list-animal component template

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -4,20 +4,57 @@ import { Animal } from './models/animal.model';
 @Component({
   selector: 'animal-list',
   template: `
-  <h1>Animals</h1>
-  <button (click)="createAnimal()">Add new animal</button>
-  <select (change)="onChange($event.target.value)">
-      <option value="1" selected="selected">All animals</option>
-      <option value="2">Young animals</option>
-      <option value="3">Mature animals</option>
-    </select>
-  <ul>
-    <li *ngFor="let animal of animals | ageCriteriaFilter:filterByAgeCriteria">
-      {{animal.name}} {{animal.age}} {{animal.caretakers}} {{animal.sex}}
-      <button (click)="editAnimal(animal)">Edit</button>
-      <button (click)="animalView(animal)">View</button>
-    </li>
-  </ul>
+  <div class="card">
+    <div class="alert alert-primary">
+    <div class="row">
+            <div class="col-md-8">
+                <div class="form-group">
+                    <h3>Animals</h3>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="form-group">
+                <select class="form-control" data-style="btn-success" (change)="onChange($event.target.value)">
+                    <option value="1" selected="selected">All animals</option>
+                    <option value="2">Young animals</option>
+                    <option value="3">Mature animals</option>
+                  </select>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="form-group">
+                <button class="btn btn-primary btn-xs" (click)="createAnimal()">New Animal</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card-body">
+    <table class="table">
+            <thead>
+            <tr>
+                <th>Species</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Caretakers Number</th>
+                <th>Admittance Date</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+              <tr *ngFor="let animal of animals | ageCriteriaFilter:filterByAgeCriteria">
+                <td>{{animal.species}}</td>
+                <td>{{animal.name}}</td>
+                <td>{{animal.age}}</td>
+                <td>{{animal.caretakers}}</td>
+                <td>{{animal.admittanceDate}}</td>
+                <td>
+                <button class="btn btn-Success btn-xs" (click)="editAnimal(animal)">Edit</button>
+                <button class="btn btn-Warning btn-xs" (click)="animalView(animal)">View</button>
+                </td>
+              </tr>
+            </tbody>
+        </table>
+    </div>
   `
 })
 
